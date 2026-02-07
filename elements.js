3df281e1cd7283c5f270057bda0fd869697f4193
iamtomij <helloiamtomij@gmail.com>
2026-02-07 22:29:53 +0600

    add or remove elements

---
+++ elements.js
@@ -0,0 +1,5 @@
+let touristPlace = ["Anderkilla", "Bandarban", "Sundarban"];
+touristPlace.push("Khagrachari");
+touristPlace.push("bichanakandi","Lalakhal");
+touristPlace.pop();
+console.log(touristPlace);
