9760ccc3481fd872dc09445cf7d2021c91f67345
iamtomij <helloiamtomij@gmail.com>
2026-02-07 23:06:13 +0600

    Combining Arrays

---
+++ combining.js
@@ -0,0 +1,8 @@
+let fruits = ["Kola","Licu","Jolpai"]
+let vegetables = ["potato","badhakopi","dheros"]
+let combinedArray=fruits.concat(vegetables);
+
+
+console.log("fruits", fruits);
+console.log("vegetables",  vegetables);
+console.log("combinedArray", combinedArray);
