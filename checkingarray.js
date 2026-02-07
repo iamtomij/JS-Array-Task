0ddccac6ca25fe0e610af9d66db5436f2393cedf
iamtomij <helloiamtomij@gmail.com>
2026-02-07 22:53:52 +0600

    Checking if it's an Array

---
+++ checkingarray.js
@@ -0,0 +1,20 @@
+// Checking if it's an Array
+// Instructions:
+
+// Create different variables, each containing either an array or a non-array value.
+
+// Now use isArray to check if each variable is an array.
+
+// Print a message to the console indicating whether each variable is an array or not.
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+let number = [1,2,3,4,5];
+let name = "Tomij"
+let numbers = 33478867234
+let isStudent = true;
+
+console.log("fruits",Array.isArray (fruits));
+console.log("number",Array.isArray (number));
+console.log("name", Array.isArray (name));
+console.log("number",Array.isArray (numbers));
+console.log("isStudent",Array.isArray(isStudent));
